3ab9dcdec2c7a9705607ccbb06b8940a59d350c8
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2025-05-21 23:40:45 +0530

    first commit

---
+++ controllers/auth/auth-controller.js
@@ -0,0 +1,94 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User.model.js");
+
+// register
+const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+
+    const checkUser =  await User.findOne({email})
+    if(checkUser) return res.json({success : false, message : "User With Give Email Already Registered, please try again."})
+
+    const hashPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      username,
+      email,
+      password : hashPassword,
+    });
+
+    await newUser.save();
+    res.status(200).json({
+      success: true,
+      message: "Registration successful",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some Error Occured",
+    });
+  }
+};
+
+// login
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const checkUser =  await User.findOne({email})
+    if(!checkUser) return res.json({success : false, message : "User doesn't exists! please register first"});
+
+      const checkPasswordMatch = await bcrypt.compare(password, checkUser.password);
+      if(!checkPasswordMatch) return res.json({success : false, message : "Incorrect password! please try again"});
+
+      const token = jwt.sign({
+        id : checkUser._id, role : checkUser.role, email : checkUser.email
+      }, 'CLIENT_SECRET_KEY', {expiresIn : '60m'})
+
+      res.cookie('token', token, {httpOnly: true, secure : false}).json({
+        success : true,
+        message : "Logged in Successful",
+        user : {
+          email : checkUser.email,
+          role : checkUser.role,
+          id : checkUser._id
+        }
+      })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some Error Occured",
+    });
+  }
+};
+
+// logout
+const logoutUser = () => {
+  res.clearCookie('token').json({
+    success : true,
+    message : "Logged out successfully!"
+  })
+}
+// auth middleware
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+  if(!token) return res.status(401).json({
+    success : false,
+    message : 'Unauthorised user!'
+  });
+  try {
+    const decoded = jwt.verify(token, 'CLIENT_SECRET_KEY');
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({
+    success : false,
+    message : 'Unauthorised user!'
+  });
+  } 
+}
+
+module.exports = {registerUser, loginUser, logoutUser, authMiddleware};
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express'); 
+const app = express();
+const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const authRoute = require("./routes/auth/auth-route")
+
+mongoose
+.connect('mongodb+srv://rahul:rahul@cluster0.l5ugu.mongodb.net/nn ')
+// .connect('mongodb+srv://rahul:rahul@cluster0.l5ugu.mongodb.net/ ')
+.then(() => console.log("DB Connected"))
+.catch((err) => console.log("Error While DB Connect : ", err))
+
+app.use(
+    cors({
+        origin : 'http://localhost:5173',
+        methods : ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders : [
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma"
+        ],
+        credentials : true
+    })
+)
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth/", authRoute);
+
+app.get("/", (req, res) => {
+    res.send("Work")
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`)
+});
