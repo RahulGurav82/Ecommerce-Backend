e26bc8b4d11c625077dac527220f33b173966d40
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2025-05-30 20:55:30 +0530

    env added

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
 
 app.use(
     cors({
-        origin : 'http://localhost:5173',
+        origin : process.env.FRONTEND_URL,
         methods : ['GET', 'POST', 'DELETE', 'PUT'],
         allowedHeaders : [
             "Content-Type",
